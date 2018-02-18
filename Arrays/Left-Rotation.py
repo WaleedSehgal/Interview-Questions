391954e4f3c3f6351f526816754754522136d2c5
WaleedSehgal <waleedsehgal@gmail.com>
2018-02-18 02:56:27 -0800

    Update Left-Rotation.py

--- Arrays/Left-Rotation.py
+++ Arrays/Left-Rotation.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Waleed
+"""
 
+a = [1,2,3,5,6,7,8]
+leftrot = 3
+
+i = 0
+while i<leftrot:
+    r = a[0]
+    a.remove(r)
+    a.append(r)
+    i+=1
+
+print(a)
