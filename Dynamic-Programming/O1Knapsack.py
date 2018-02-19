14d22a9800a337c546d28a95c60fc72933de3829
WaleedSehgal <waleedsehgal@gmail.com>
2018-02-19 05:53:08 -0800

    Create O1Knapsack.py

---
+++ Dynamic-Programming/O1Knapsack.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Waleed
+"""
+
+cap = 50
+value = [200,10000,1000,120]
+weight = [10,50,20,30]
+n = len(weight)
+
+valmax = [[None for x in range(cap+1)] for y in range(n+1)]
+
+def KnapSack(n, cap):
+    
+    if valmax[n][cap]!=None:
+        return valmax[n][cap]
+    
+    if n==0 or cap==0:
+        return 0
+    
+    elif weight[n-1]>cap:
+        result = KnapSack(n-1, cap)
+    
+    else:
+        
+        t1 = KnapSack(n-1,cap)
+        t2 = value[n-1] + KnapSack(n-1, cap-weight[n-1])
+        result = max(t1,t2)
+        
+    valmax[n][cap] = result
+    return result        
+
+
+print(KnapSack(n,cap))
