9ed476426da1841ea33d3f11b521229bcfd1e7d8
WaleedSehgal <waleedsehgal@gmail.com>
2018-02-19 20:07:41 -0800

    Create SellBuyWfee.py

---
+++ Dynamic-Programming/SellBuyWfee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Waleed
+
+Calculate best time to sell and buy stocks with a tranaction fee with every transaction.
+"""
+
+profit = 0
+buy = -(prices[0])
+        
+for i in range(1,len(prices)):
+            
+    profit = max(profit, ns+prices[i]-fee)
+    buy = max(ns, profit-prices[i])
+
+return profit
