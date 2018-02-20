e14ad856b643f83e45238c6826b90799a5630357
WaleedSehgal <waleedsehgal@gmail.com>
2018-02-20 02:07:37 -0800

    Create ParteqSum.py

---
+++ Dynamic-Programming/ParteqSum.py
@@ -0,0 +1,20 @@
+"""
+@author: Waleed
+
+Check wether a given array can be partitions into two arrays of equal sums.
+"""
+
+arr = [1,2,3,4,5,6,7]
+    
+target = sum(arr)/2
+
+sums = {0}
+
+#store sums of all subsets for each i in arr
+for i in range(len(arr)):
+    sums.update({(j+arr[i]) for j in sums})
+
+if target in sums:
+    print('Yes')
+else:
+    print('No')
