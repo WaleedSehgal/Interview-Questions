5d6b402ced6266f09f380756273bbba6c8390150
WaleedSehgal <waleedsehgal@gmail.com>
2018-02-20 04:25:05 -0800

    Create SubinString.py

---
+++ Strings/SubinString.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Waleed
+Find the substring in the string and return index of first character's occurence.
+"""
+
+s = 'Find the substring in this string'
+ss = 'the sub'
+
+ssl = len(ss)
+
+for i,c in enumerate(s):
+    
+    if c == ss[0]:
+        sub = s[i:i+ssl] 
+        if sub==ss:
+            print(i)
+            break
