d11fb727fc1736ab0d7cd309c69c322f328804e9
WaleedSehgal <waleedsehgal@gmail.com>
2018-02-20 21:18:51 -0800

    Create findPath.py

---
+++ Graphs/findPath.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Waleed
+
+returns path if their exists one from the given start node to the end node.
+"""
+
+graph = {'a':['b'],
+         'b':['a','c'],
+         'c':['c','d'],
+         'd':['b']
+         }
+
+def findpath(graph, start, end, path=[]):
+    
+    path.extend(start)
+    
+    if start==end:
+        return path
+    
+    if start not in graph.keys():
+        return 'start node not in graph'
+    
+    for node in graph[start]:
+        
+        if node not in path:
+            p = findpath(graph, node, end, path)   
+            if p:
+                return p
+    return 'No path found'
+
+print(findpath(graph, 'a', 'd', path=[]))
+print(findpath(graph, 'd', 'a', path=[]))
